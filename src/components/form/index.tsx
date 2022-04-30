4ea306a16ab6d64e5c681998326bbd1f4512e60a
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2022-04-30 17:08:24 -0300

    falta validar form e arrumar ui
    falta validar form e arrumar ui

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -11,26 +11,65 @@ import {
   Stack,
   Input,
 } from "@chakra-ui/react";
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  OptionHTMLAttributes,
+  useEffect,
+  useState,
+} from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
-type Chamada = {
-  origem: String;
-  destino: String;
-  duracao: Number;
-  plano: String;
+type FormData = {
+  origem?: String;
+  destino?: String;
+  duracao?: String;
+  plano?: String;
+};
+
+type Cost = {
+  custoComPlano?: Number;
+  custoSemPlano?: Number;
 };
 
 type Response = {
-  custoComPlano: Number;
-  custoSemPlano: Number;
+  Ok: Cost;
 };
 
 export const Form = () => {
-  const { handleSubmit, register } = useForm<Chamada>();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
+  const [callValue, setCallValue] = useState<Response>();
+  const [cost, setCost] = useState<Response>();
 
-  const handleFetch = async ({ origem, destino, duracao, plano }: Chamada) => {
-    return fetch(
-      `/tarifa?origem=${origem}&destino=${destino}&duracao=${duracao}&plano=${plano}`,
+  const [selected, setSelected] = useState<String>();
+  const [DDD, setDDD] = useState<boolean>();
+
+  function handleChange(event: ChangeEvent<HTMLSelectElement>) {
+    const selected = event.target.options[event.target.selectedIndex].value;
+
+    console.log(selected);
+
+    selected === "011" ? setDDD(true) : setDDD(false);
+    setSelected(selected);
+  }
+
+  useEffect(() => {
+    setCost(callValue);
+  }, [callValue]);
+
+  async function handleFetch({
+    origem,
+    destino,
+    duracao,
+    plano,
+  }: FormData): Promise<Response> {
+    const response = await fetch(
+      `http://localhost:3333/tarifa?origem=${origem}&destino=${destino}&duracao=${duracao}&plano=${plano}`,
       {
         method: "GET",
         headers: {
@@ -44,7 +83,11 @@ export const Form = () => {
       .catch((err) => {
         console.log(err);
       });
-  };
+
+    setCallValue(response);
+
+    return response;
+  }
 
   return (
     <VStack>
@@ -53,7 +96,7 @@ export const Form = () => {
       <Stack
         w="100%"
         border={"1px"}
-        borderColor={"gray.200"}
+        borderColor={"gray"}
         borderRadius={"lg"}
         p={5}
       >
@@ -64,6 +107,7 @@ export const Form = () => {
               id="origem"
               placeholder="Selecione o DDD de origem"
               {...register("origem", { required: true })}
+              onChange={handleChange}
             >
               <option value={"011"}>011</option>
               <option value={"016"}>016</option>
@@ -79,10 +123,15 @@ export const Form = () => {
               placeholder="Selecione o DDD de destino"
               {...register("destino", { required: true })}
             >
-              <option value={"011"}>011</option>
-              <option value={"016"}>016</option>
-              <option value={"017"}>017</option>
-              <option value={"018"}>018</option>
+              {selected && DDD ? (
+                <>
+                  <option value={"016"}>016</option>
+                  <option value={"017"}>017</option>
+                  <option value={"018"}>018</option>
+                </>
+              ) : (
+                <option value={"011"}>011</option>
+              )}
             </Select>
           </FormControl>
 
@@ -101,6 +150,20 @@ export const Form = () => {
                 },
               })}
             />
+            {errors.duracao && <Text color="red">{errors.duracao.errors}</Text>}
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="plano">Plano</FormLabel>
+            <Select
+              id="plano"
+              placeholder="Selecione o plano"
+              {...register("plano", { required: true })}
+            >
+              <option value={"FaleMais 30"}>FaleMais 30</option>
+              <option value={"FaleMais 60"}>FaleMais 60</option>
+              <option value={"FaleMais 120"}>FaleMais 120</option>
+            </Select>
           </FormControl>
 
           <Box w={"100%"}></Box>
@@ -112,7 +175,7 @@ export const Form = () => {
               </Text>
 
               <Box>
-                <Text fontSize={"lg"}>0</Text>
+                <Text fontSize={"lg"}>{cost?.Ok && cost.Ok.custoComPlano}</Text>
               </Box>
             </VStack>
 
@@ -122,7 +185,7 @@ export const Form = () => {
               </Text>
 
               <Box>
-                <Text fontSize={"lg"}>0</Text>
+                <Text fontSize={"lg"}>{cost?.Ok && cost.Ok.custoSemPlano}</Text>
               </Box>
             </VStack>
           </HStack>
@@ -133,7 +196,7 @@ export const Form = () => {
             _hover={{ opacity: "0.6" }}
             type={"submit"}
           >
-            Calcular ligação
+            Calcular chamada
           </Button>
         </form>
       </Stack>
